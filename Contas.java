a204c7b1d864e2216c20432ce55d05bbf639183d
GEWPTHEKILLERSM <87881165+GEWPTHEKILLERSM@users.noreply.github.com>
2022-04-04 10:25:45 -0300

    e
    f

---
+++ Contas.java
@@ -0,0 +1,48 @@
+package p3_UNIKUT;
+
+import java.util.ArrayList;
+
+public class Contas {
+    private ArrayList<Conta> contas = new ArrayList<>();
+
+    private boolean isEmpty(){
+        if(this.contas.size() == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private boolean verifyLogin(Conta c){
+
+        for(int i = 0; i < contas.size() - 1; i++){
+            if(contas.get(i).equals(c)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void newAccount(Conta c){
+
+        if(isEmpty()){
+
+            contas.add(c);
+
+        }else{
+            if(verifyLogin(c)){
+                System.out.println("Login em uso");
+            }else{
+                contas.add(c);
+            }
+
+        }
+
+    }
+
+    public void exibirContas(){
+        for(int i = 0; i < contas.size() - 1; i++){
+            System.out.println(contas.get(i));
+        }
+    }
+}
