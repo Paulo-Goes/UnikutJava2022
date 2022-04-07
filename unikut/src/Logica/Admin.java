49987463236856aa51b87fd87308798e93a42952
GEWPTHEKILLERSM <paulosergiolobogoes@gmail.com>
2022-04-07 09:47:52 -0300

    e

--- unikut/src/Logica/Admin.java
+++ unikut/src/Logica/Admin.java
@@ -4,7 +4,6 @@ public class Admin extends Conta{
     private String login;
     private String senha;
     public Admin(String login, String senha) {
-        this.login = login;
-        this.senha = senha;
+        super(login, senha);
     }
 }
