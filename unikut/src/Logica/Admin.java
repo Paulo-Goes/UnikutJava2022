c5f606926386558f0e15bfd1ca0adc5c1d98c616
PC UNICAP <loyola@unicap.br>
2022-04-07 10:26:44 -0300

    Construtor na classe ADMIN

--- unikut/src/Logica/Admin.java
+++ unikut/src/Logica/Admin.java
@@ -1,9 +1,11 @@
 package Logica;
 
 public class Admin extends Conta{
-    private String login;
-    private String senha;
     public Admin(String login, String senha) {
-        super(login, senha);
+        super("ADMIN", "paofrances");
+    }
+
+    public void changeLogin(String c){
+        
     }
 }
