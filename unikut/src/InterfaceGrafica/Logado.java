f35d6a597d0d174d10cad978d812f98892ca262f
PC UNICAP <loyola@unicap.br>
2022-04-11 10:02:26 -0300

    Mudanças Logado

--- unikut/src/InterfaceGrafica/Logado.java
+++ unikut/src/InterfaceGrafica/Logado.java
@@ -1,6 +1,23 @@
 package InterfaceGrafica;
 
-public class Logado {
+import java.util.Locale;
+import java.util.Scanner;
+import Logica.Contas;
+
+public class Logado extends Contas {
+    public static void main (Contas contas) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("/Interface quando logado/");
 
 
+    }
+
+    static void menuUI() {
+        System.out.println("| UNIKUT SOCIAL MEDIA © | ");
+        System.out.println("1 - Adicionar amigos");
+        System.out.println("2 - Depoimentos");
+        System.out.println("0 - Sair");
+    }
 }
