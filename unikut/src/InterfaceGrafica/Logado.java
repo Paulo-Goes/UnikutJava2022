07d2aa422f1ff7ee6df14bdc8944587c098e9a04
PC UNICAP <loyola@unicap.br>
2022-04-11 10:48:05 -0300

    Mudanças Logado

--- unikut/src/InterfaceGrafica/Logado.java
+++ unikut/src/InterfaceGrafica/Logado.java
@@ -4,20 +4,37 @@ import java.util.Locale;
 import java.util.Scanner;
 import Logica.Contas;
 
-public class Logado extends Contas {
+public class Logado {
     public static void main (Contas contas) {
 
         Scanner input = new Scanner(System.in);
 
+        char menuInput;
+
         System.out.println("/Interface quando logado/");
+        // Menu de ações iniciais
+        do {
+            menuUI();
+            menuInput = input.next().charAt(0);
+            input.nextLine();
 
+            if (menuInput == '2') {
+                if (contas.checkLoginDisponibility(String login) == false) {
 
+                }
+            }
+            // Exibição caso a opção não esteja no menu
+            System.out.println("Ocorreu um erro: Opção invalida");
+        } while (menuInput != '0');
+        // Finaliza scanner input
+        input.close();
     }
 
     static void menuUI() {
         System.out.println("| UNIKUT SOCIAL MEDIA © | ");
-        System.out.println("1 - Adicionar amigos");
-        System.out.println("2 - Depoimentos");
+        System.out.println("1 - Edição de perfil");
+        System.out.println("2 - Adicionar amigos");
+        System.out.println("3 - Depoimentos");
         System.out.println("0 - Sair");
     }
 }
