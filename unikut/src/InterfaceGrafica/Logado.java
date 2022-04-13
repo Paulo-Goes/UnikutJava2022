97e0afc50b113743ef0ffc91f7e90f5ac29ff11e
bnour1 <brunowashingtong@gmail.com>
2022-04-13 19:48:57 -0300

    Implementa opção de mudar Nome

--- unikut/src/InterfaceGrafica/Logado.java
+++ unikut/src/InterfaceGrafica/Logado.java
@@ -1,40 +1,37 @@
 package InterfaceGrafica;
 
-import java.util.Locale;
 import java.util.Scanner;
-import Logica.Contas;
+import Logica.*;
 
 public class Logado {
-    public static void main (Contas contas) {
+    public static void main (Conta loggedAccount) {
 
         Scanner input = new Scanner(System.in);
 
         char menuInput;
 
-        System.out.println("/Interface quando logado/");
+        
         // Menu de ações iniciais
         do {
             menuUI();
             menuInput = input.next().charAt(0);
             input.nextLine();
 
-            if (menuInput == '2') {
-                if (contas.checkLoginDisponibility(String login) == false) {
-                    //Lógica adicionar amigos
-                }
-                else {
-
-                }
+            switch(menuInput) {
+                case '1':
+                ProfileSettings.main(loggedAccount);
             }
-            // Exibição caso a opção não esteja no menu
-            System.out.println("Ocorreu um erro: Opção invalida");
+            
         } while (menuInput != '0');
         // Finaliza scanner input
-        input.close();
+    
     }
 
     static void menuUI() {
-        System.out.println("| UNIKUT SOCIAL MEDIA © | ");
+        System.out.println("< > unikut.com/Home/");
+        System.out.println();
+        System.out.println();
+        System.out.println("Home");
         System.out.println("1 - Edição de perfil");
         System.out.println("2 - Adicionar amigos");
         System.out.println("3 - Depoimentos");
