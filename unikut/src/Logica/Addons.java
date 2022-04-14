6ab5e53c799308affc97f7d020e6f6435e6d5878
bnour1 <brunowashingtong@gmail.com>
2022-04-13 21:29:36 -0300

    Metodo para por delay entre processos

--- unikut/src/Logica/Addons.java
+++ unikut/src/Logica/Addons.java
@@ -2,12 +2,23 @@ package Logica;
 
 public class Addons {
 
-        public static void delay(int timeMS){
-            try {
-                Thread.sleep(timeMS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
+    // Insere delay entre os processos
+    public static void delay(int op) {
+        int timeMS = 0;
+
+        if (op == 1) { // Delay 1: 350MS
+            timeMS = 350;
+        } else if (op == 2) { // Delay 2: 500MS
+            timeMS = 500;
+        } else if (op == 3) { // Delay 3: 1500MS
+            timeMS = 1500;
         }
+        try {
+            Thread.sleep(timeMS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
