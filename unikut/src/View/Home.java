c69a8923fb45cca9e2ca096ab707218872a98bfc
bnour1 <brunowashingtong@gmail.com>
2022-05-19 15:46:13 -0300

    Melhorias mesclagens simplificações e throws

--- unikut/src/View/Home.java
+++ unikut/src/View/Home.java
@@ -1,13 +1,15 @@
 package View;
 
 import java.util.Scanner;
-
+import java.util.Locale;
 import Controller.Addons;
-import Controller.User;
-import Model.SocialNetwork;
+import Controller.SocialNetwork;
+import Model.User;
+import java.util.InputMismatchException;
+import Exceptions.*;
 
 public class Home {
-    public static void main(User loggedAccount) {
+    public static void main(User loggedAccount) throws Exception {
         SocialNetwork unikut = Main.unikut;
 
         Scanner input = new Scanner(System.in);
@@ -23,7 +25,7 @@ public class Home {
             switch (menuInput) {
                 case '1': // Edição de perfil
                     Addons.delay(1);
-                    ProfileSettings.main(loggedAccount);
+                    profileSettings(loggedAccount);
                     break;
                 case '2': // Adição de amigos
                     Addons.delay(1);
@@ -43,7 +45,11 @@ public class Home {
                     Addons.delay(2);
                     break;
                 case '6': // Exibir Menssagens
+                try{
                     unikut.showMessages(loggedAccount);
+                }catch(final EmptyInbox e){
+                    System.out.println("Não hà novos depoimentos");
+                }
                     Addons.delay(2);
                     break;
                 default:
@@ -53,6 +59,7 @@ public class Home {
 
     }
 
+    // Exibir opções para usuario logado
     static void menuUI() {
         System.out.println("\n< > unikut.com/Home/\n");
         System.out.println("Home");
@@ -66,6 +73,7 @@ public class Home {
         System.out.println("0 - Sair");
     }
 
+    // Adicionar amigo
     static void addFriendsUI(SocialNetwork unikut, User loggedAccount) {
         try (Scanner in = new Scanner(System.in)) {
             String friendLogin;
@@ -82,18 +90,132 @@ public class Home {
 
     }
 
-    static void sendMessageUI(SocialNetwork unikut, User loggedAccount) {
-        try (Scanner input = new Scanner(System.in)) {
-            String message, loginRecpetor;
+    // Envio de menssagem
+    static void sendMessageUI(SocialNetwork unikut, User loggedAccount) throws Exception {
+        Scanner input = new Scanner(System.in);
+        String message, loginRecpetor;
 
-            System.out.println("Insira o login do destinatario");
-            loginRecpetor = input.next();
-            input.nextLine();
+        System.out.println("Insira o login do destinatario");
+        loginRecpetor = input.next();
 
-            System.out.println("Digite a menssagem abaixo: ");
-            message = input.nextLine();
-
+        System.out.println("Digite a menssagem abaixo: ");
+        message = input.nextLine(); message = input.nextLine();
+        try{
             unikut.sendMessage(message, loggedAccount, loginRecpetor);
+        }catch(NullPointerException e){
+            System.out.println("User dosn't Exist");
         }
+    }
+
+    // Edição de perfil
+    public static void profileSettings(User editingAccount) throws Exception {
+        Scanner in = new Scanner(System.in);
+        char op;
+            // Interface de opções
+        do {
+            optionsUI(editingAccount); // Exibe opções de edição de perfil
+            op = in.next().toLowerCase(Locale.ROOT).charAt(0);
+            in.nextLine();
+            switch (op) {
+                case '1': // Mudarnça de nome
+                    Addons.delay(1);
+                    changeNameUI(editingAccount);
+                    break;
+                case '2': // Mudança de senha
+                    Addons.delay(1);
+                    changePasswordUI(editingAccount);
+                    break;
+                default: // Reiniciar menu se a opção não estiver listada
+                    System.out.println("Oops, essa opção ainda não existe :/");
+                    Addons.delay(1);
+                    break;
+            }
+        } while (op != '0');
+    }
+    
+    // Exibir opções de edição
+    static void optionsUI(User loggedAccount) { 
+        System.out.println("\n< > unikut.com/" + loggedAccount.getLogin() + "/edit");
+        System.out.println();
+        System.out.println("Edição de perfil");
+        System.out.println("1 - Alterar nome de exibição");
+        System.out.println("2 - Alterar senha");
+        System.out.println("0 - Retornar à home");
+    }
+    
+    // Interface de mudança de nome no unikut
+    static void changeNameUI(User loggedAccount) { 
+        try (Scanner in = new Scanner(System.in)) {
+            String newName;
+            int op;
+            System.out.println("\nMudar nome de exibição \n Insira seu novo nome de exibição");
+            newName = in.nextLine();
+            System.out.println("Seu nome de exibição atual é: " + loggedAccount.getNome()
+                    + "\n\nSeu novo nome de exibição será: " + newName);
+            Addons.delay(3);
+            System.out.println("1 - Confirmar mudança\n2 - Descatar mudança");
+            
+            //Tratamento de um possivel exception
+            try {
+                op = in.nextInt();
+                if (op == 1) { // Condicionais para confirmar a alteração de nome
+                    loggedAccount.setNome(newName);
+                    System.out.println("Mudança efetuada, " + loggedAccount.getNome() + " :)");
+                    Addons.delay(1);
+                } else if (op == 2){
+                    System.out.println("Nome de exibição mantido.");
+                    Addons.delay(1);
+                }
+                else {
+                    System.out.println("Opção invalida, digite uma opção válida.");
+                }
+                //Tratamento de um possivel exception
+            } catch (InputMismatchException e){
+                System.err.println("Opção inválida. Digite 1 ou 2 para validar o processo, retornando para o menu anterior.");
+            }
+        }
+    }
+    
+    // Interface de mudança de senha
+    static void changePasswordUI(User loggedAccount) throws Exception { 
+        try (Scanner in = new Scanner(System.in)) {
+            String newPassword, newPasswordCheckout;
+            int op;
+            do {
+                System.out.println("Insira sua nova senha");
+                newPassword = in.next();
+                in.nextLine();
+                System.out.println("Insira sua nova senha novamente");
+                newPasswordCheckout = in.next();
+                in.nextLine();
+            } while (!newPassword.equals(newPasswordCheckout)); // Força a igualdade entre as duas senhas iseridas acinma
+            System.out.println("Tem certeza que deseja alterar a senha, " + loggedAccount.getNome() + " ?");
+            Addons.delay(3);
+            System.out.println("1 - Sim c:\n2 - Não :c");
+            //Tratamento de um possivel exception
+            try {
+                op = in.nextInt();
+                if (op == 1) {
+                    try{
+                        Main.unikut.changePassword(loggedAccount, newPassword); // Metodo que muda a senha na classe contas
+                    }catch(final WeakPassword e){
+                        System.out.println("your password must contain: \nMore than 6 characters\nAt least one Symbol\nAt least one number\nUpper and Lower cases characters");
+                    }
+                    
+                    System.out.println("Senha alterada com sucesso :)");
+                } else if (op == 2){
+                    System.out.println("A mudança foi descartadas Mr. " + loggedAccount.getNome() + " :)");
+                }
+                else if (op == 0){
+                    System.out.println("Retornando ao menu anterior.");
+                }else {
+                    System.out.println("Opção invalida, digite uma opção válida.");
+                }
+                //Tratamento de um possivel exception
+            }catch (InputMismatchException e){
+                System.err.println("Opção inválida. Digite 1 ou 2 para validar o processo, retornando para o menu anterior.");
+            }
+        }
+        Addons.delay(2);
     }
 }
--- unikut/src/View/Main.java
+++ unikut/src/View/Main.java
@@ -1,14 +1,14 @@
 package View;
 
-import java.util.Locale;
 import java.util.Scanner;
 
-import Controller.Addons;
-import Model.SocialNetwork;
+import java.util.Locale;
+import Controller.*;
 
 public class Main {
     public static SocialNetwork unikut = new SocialNetwork();
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         char menuInput;
         // Interface de opções
@@ -20,7 +20,7 @@ public class Main {
             switch (menuInput) {
                 case '1': // Interface de criação de conta na classe SignUp
                     Addons.delay(1);
-                    SignUp.main(unikut);
+                    signUpUI();
                     break;
                 case '2': // Logica de login no método loginUI
                     Addons.delay(1);
@@ -30,14 +30,15 @@ public class Main {
                     System.out.println("/Lista de usuarios cadastrados/");
                     unikut.showAccounts();
                     break;
-            default: // Reiniciar menu se a opção não estiver listada
-                break;
+                default: // Reiniciar menu se a opção não estiver listada
+                    break;
             }
         } while (menuInput != '0');
-    
+
         System.out.println("Foi bom enquanto durou :/");
         Addons.delay(2);
     }
+
     // Método que exibe as opções do menu
     static void menuUI() {
         System.out.println("\n< > unikut.com");
@@ -47,38 +48,97 @@ public class Main {
         System.out.println("2 - Login");
         System.out.println("0 - Sair");
     }
+
     // Interface de login
-    static void loginUI(SocialNetwork unikut) {
+    static void loginUI(SocialNetwork unikut) throws Exception {
         String loginInput, senhaInput;
-        char op;
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.println("Voltar para o menu principal?");
-            System.out.println("S/N");
-            op = in.next().toLowerCase(Locale.ROOT).charAt(0);
-            while (op != 's' && op != 'n') {
-                System.out.println("Erro, digite uma opção válida!");
-                System.out.println("Voltar para o menu principal?");
-                System.out.println("S/N");
-                op = in.next().toLowerCase(Locale.ROOT).charAt(0);
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Conecte-se usando suas credenciais");
+        do {
+            System.out.println("Insira o login: ");
+            loginInput = in.next();
+            in.nextLine();
+            System.out.println("Insira a senha: ");
+            senhaInput = in.next();
+            in.nextLine();
+            if (unikut.login(loginInput, senhaInput) == null) {
+                System.out.println("Suas credencias não foram validadas.");
             }
-            if(op == 'n'){
-            System.out.println("Conecte-se usando suas credenciais");
-            do {
-                    System.out.println("Insira o login: ");
-                    loginInput = in.next();
-                    in.nextLine();
-                    System.out.println("Insira a senha: ");
-                    senhaInput = in.next();
-                    in.nextLine();
-                    if (unikut.login(loginInput, senhaInput) == null){
-                        System.out.println("Suas credencias não foram validadas.");
-                    }
-            } while (unikut.login(loginInput, senhaInput) == null); // função que retorna uma conta se as credencias
-                                                                    // corresponderem a uma conta cadastrada
-                System.out.println("Login efetuado com sucesso!");
-                Addons.delay(1);
-                Home.main(unikut.login(loginInput, senhaInput)); // vai para a interface de login
+        } while (unikut.login(loginInput, senhaInput) == null); // função que retorna uma conta se as credencias
+                                                                // corresponderem a uma conta cadastrada
+        System.out.println("Login efetuado com sucesso!");
+        Addons.delay(1);
+        Home.main(unikut.login(loginInput, senhaInput)); // vai para a interface de login
+
+    }
+
+    // Interface de cadastro
+    static void signUpUI() {
+        Scanner input = new Scanner(System.in);
+            String nome, login, senha;
+            char nameOptionInput;
+
+            System.out.println("\nCriação de conta");
+
+            // Solicita um login ao usuario
+            System.out.println("Insira um Login");
+            login = input.next();
+            input.nextLine();
+
+            // Força o usuario a inserir um login que não foi utilizado
+            while (unikut.search(login) != null) {
+                System.out.println("O login informado já está sendo usado :/");
+                Addons.delay(350);
+                System.out.println("Insira um login: ");
+                login = input.next();
             }
-        }
+
+            // Solicita uma senha ao usuario como os critérios informados
+            System.out.println("Criação de senha:");
+            System.out.println("* Mais do que 6 caracteres");
+            System.out.println("* Letras maiúsculas e minúsculas");
+            System.out.println("* Números");
+            System.out.println("* Caracteres especiais");
+            System.out.println("Insira uma senha forte:");
+            senha = input.next();
+            input.nextLine();
+            
+            // Usuário é forçado à criar uma conta com os critérios informados
+            while (!unikut.senhaForte(senha)) {
+                System.out.println("Senha muito fraca :/");
+                Addons.delay(350);
+                System.out.println("Insira outra senha: ");
+                senha = input.next();
+            }
+
+            // Pergunta se o usuario deseja adicionar um nome
+            System.out.println("Deseja inserir o nome? \n S/N");
+            nameOptionInput = input.next().toLowerCase(Locale.ROOT).charAt(0);
+            input.nextLine();
+
+            // Força o usuario a inserir apenas S ou N em nameOptionInput
+            while (nameOptionInput != 's' && nameOptionInput != 'n') {
+                System.out.println("Oops, o caracter inserido deve ser [S] ou [N] (not case sensetive)");
+                Addons.delay(350);
+                System.out.println("Deseja inserir o nome \n S/N");
+                nameOptionInput = input.next().toLowerCase(Locale.ROOT).charAt(0);
+                input.nextLine();
+            }
+
+            // Logica para vincular um nome a conta ou definir nome como "convidado"
+            if (nameOptionInput == 's') {
+                System.out.println("Insira o nome");
+                nome = input.nextLine();
+            } else {
+                nome = "convidado";
+            }
+
+            System.out.println("Conta criada com sucesso! use suas credenciais para fazer login na proxima vez :)");
+            Addons.delay(2);
+
+            // Metodo para criar a conta
+            unikut.createAccount(nome, login, senha);
     }
 }
